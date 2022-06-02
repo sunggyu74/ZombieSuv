0708c9a1a80ed72361cd72d67f5c4dc509a92baf
허성규 <fdrack@naver.com>
2022-06-02 11:04:06 +0900

    20220602
    좀비 클라이언트.

---
+++ zCl_20220602/Assets/02.Scripts/AmmoPack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 탄알을 충전하는 아이템
+public class AmmoPack : MonoBehaviour, IItem
+{
+    public int ammo = 30; // 충전할 탄알 수
+
+    public void Use(GameObject target)
+    {
+        // 전달받은 게임 오브젝트로부터 PlayerShooter 컴포넌트 가져오기 시도
+        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
+
+        // PlayerShooter 컴포넌트가 있으며 총 오브젝트가 존재하면
+        if (playerShooter != null && playerShooter.gun != null){
+            // 총의 남은 탄알 수를 ammo만큼 더함
+            playerShooter.gun.__Remain += ammo;
+
+        }
+
+        // 사용되었으므로 자신을 파괴
+        Destroy(gameObject);
+    }
+
+}
